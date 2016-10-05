bcc49d18972eb648d3e0a8877e45f6f2606fd8db
Eric jonas <jonas@ericjonas.com>
2016-10-05 10:27:49 -0700

    try to find eigen from eigency

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 import eigency
 from distutils.core import setup
+#import pkgconfig
 
 from distutils.extension import Extension
 from Cython.Build import cythonize
@@ -10,13 +11,13 @@ from Cython.Build import cythonize
 #os.environ["CC"] = "g++-5" 
 #os.environ["CXX"] = "g++-5"
 
+eigen_include = "" # pkgconfig.cflags('eigen3')
     
 extensions = [Extension(name = "chirpz.cychirpz",
                         sources = ["chirpz/cychirpz.pyx", "chirpz/chirpz.cc"],
-                        extra_compile_args = ['-O3', '-DEIGEN_NO_AUTOMATIC_RESIZING', '-march=native', '-std=c++14'],
-                        include_dirs = [np.get_include(), "./",
-                                   "/usr/local/include/eigen3/"] \
-                        + eigency.get_includes(include_eigen=False), 
+                        extra_compile_args = ['-O3', '-DEIGEN_NO_AUTOMATIC_RESIZING', '-march=native', '-std=c++14', eigen_include],
+                        include_dirs = [np.get_include(), "./",] \
+                        + eigency.get_includes(include_eigen=True), 
                         extra_link_args = ['-lm', '-lfftw3f', '-lboost_system', '-lboost_timer'], 
                         language='c++')
 ]
