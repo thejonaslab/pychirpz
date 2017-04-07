2e7d9bc2a21d1082495807ef786ee0fd24084b24
Eric Jonas <jonas@ericjonas.com>
2017-04-07 13:22:07 -0700

    correct invocation

--- tests/setup_osx.sh
+++ tests/setup_osx.sh
@@ -3,7 +3,9 @@
 set -ev
 
 brew update
-brew install fftw boost
+brew install fftw || true
+# if it's already installed it will fail, so we always return success
+brew install boost || true 
 
 wget https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh -O miniconda.sh;
 
