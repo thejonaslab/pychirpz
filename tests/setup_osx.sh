4f087f1542bf74013875ac97df912f99152cb273
Eric Jonas <jonas@ericjonas.com>
2017-04-07 12:32:08 -0700

    travis does not officially support python

--- tests/setup_osx.sh
+++ tests/setup_osx.sh
@@ -5,10 +5,6 @@ set -ev
 brew update
 brew install fftw boost
 
-if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
-      wget https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh -O miniconda.sh;
-    else
-      wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
-    fi
+wget https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh -O miniconda.sh;
 
 exit 0;
