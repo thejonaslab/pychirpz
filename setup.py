5c8d6c7e67066421c9cd202c22d055da7f06265f
Eric Jonas <jonas@ericjonas.com>
2017-04-07 09:36:34 -0700

    clean up linux/osx deps

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import numpy as np
 import os
+import sys
 import eigency
 from distutils.core import setup
 #import pkgconfig
@@ -11,12 +12,18 @@ from Cython.Build import cythonize
 #os.environ["CC"] = "g++-5" 
 #os.environ["CXX"] = "g++-5"
 
+COMPILE_ARGS = ['-O3', '-DEIGEN_NO_AUTOMATIC_RESIZING', 
+                '-march=native', '-std=c++11']
+
+if sys.platform == 'darwin':
+    COMPILE_ARGS.append('-stdlib=libc++')
+
 extensions = [Extension(name = "chirpz.cychirpz",
                         sources = ["chirpz/cychirpz.pyx", "chirpz/chirpz.cc"],
-                        extra_compile_args = ['-O3', '-DEIGEN_NO_AUTOMATIC_RESIZING', '-march=native', '-std=c++11'],
+                        extra_compile_args =COMPILE_ARGS, 
                         include_dirs = [np.get_include(), "./",] \
                         + eigency.get_includes(include_eigen=True), 
-                        extra_link_args = ['-lm', '-lfftw3f', '-lboost_system', '-lboost_timer', '-lstdc++'], 
+                        extra_link_args = ['-lm', '-lfftw3f', '-lboost_system', '-lboost_timer'], 
                         language='c++')
 ]
 
